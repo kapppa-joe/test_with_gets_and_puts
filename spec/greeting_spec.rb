760b25a3a360380f159e7cae26a15316b4866578
Joe Fong <joe.fong@madetech.com>
2022-02-15 21:06:21 +0000

    pass 1st test

--- spec/greeting_spec.rb
+++ spec/greeting_spec.rb
@@ -1,9 +1,9 @@
 require 'greeting'
 
-describe "Greeting#ask_for_user_name" do
+describe 'Greeting#ask_for_user_name' do
   it "prints 'Hello! What is your name? to the console'" do
     greeting = Greeting.new
-    expected_msg = 'Hello! What is your name?'
-    expect { greeting.ask }.to output(expected_msg).to_stdout
+    expected_msg = "Hello! What is your name?\n"
+    expect { greeting.ask_for_user_name }.to output(expected_msg).to_stdout
   end
 end
